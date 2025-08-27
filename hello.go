2dcaa91b9f210dff79ab4c642578a5fffb996de1
Raúl Luis <soyrauluis@gmail.com>
2025-08-27 01:28:13 -0400

    hello with name
    Discipline:
    
    - Write a test
    - Make the compiler pass
    - Run the test, see that it fails and check the error message is
      meaningful
    - Write enough code to make the test pass
    - Refactor

---
+++ hello.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+const (
+	spanish = "Spanish"
+	french  = "French"
+
+	englishHelloPrefix = "Hello, "
+	spanishHelloPrefix = "Hola, "
+	frenchHelloPrefix  = "Bonjour, "
+)
+
+func Hello(name string, language string) string {
+	if name == "" {
+		name = "World"
+	}
+
+	return gettingPrefix(language) + name
+}
+
+func gettingPrefix(language string) (prefix string) {
+	switch language {
+	case spanish:
+		prefix = spanishHelloPrefix
+	case french:
+		prefix = frenchHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
+}
+
+func main() {
+	fmt.Println(Hello("world", ""))
+}
